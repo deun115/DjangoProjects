c72eac7c494d2eb8e6b0db4751d81e901e678ec7
deun115 <diddmstj15@naver.com>
2023-11-05 20:37:32 +0900

    Adding Vote, Testing serializers, and Testing Views

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.contrib.auth.models import User
 import datetime
 
 class Question(models.Model):
@@ -20,10 +21,21 @@ class Question(models.Model):
         return f'{new_badge} 제목: {self.question_text}, 날짜: {self.pub_date}'
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    # def __str__(self):
+    #     return f'{[self.question.question_text]}{self.choice_text}'
     def __str__(self):
-        return f'{[self.question.question_text]}{self.choice_text}'
-# Create your models here.
+        return self.choice_text
+    
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields={'question', 'voter'}, name='unique_voter_for_questions')
+        ]
